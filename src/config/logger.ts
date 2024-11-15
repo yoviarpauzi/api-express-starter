fd5d24b020766bbe03b4ffd2d692f143ab24ba9b
yoviarpauzi <yoviarpauzi99@gmail.com>
2024-11-16 00:35:17 +0700

    feat(config): add format for logger

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -1,15 +1,25 @@
-import winston, { log } from "winston";
+import winston from "winston";
 import DailyRotateFile from "winston-daily-rotate-file";
 
-const logger = winston.configure({
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.combine(
+    winston.format.colorize({ all: true }),
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
   transports: [
-    new winston.transports.DailyRotateFile({
-      level: "info",
+    new DailyRotateFile({
+      level: "error",
       filename: "application-%DATE%.log",
       datePattern: "YYYY-MM-DD-HH",
       zippedArchive: true,
       maxSize: "20m",
       maxFiles: "14d",
+    }),
+    new winston.transports.Console({
+      level: "info",
+      format: winston.format.simple(),
     }),
   ],
 });
